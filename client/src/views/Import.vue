d2aac26373b1960f66ca254c1db0299bc24f3ef9
moog.kwon <diligentaf@gmail.com>
2022-09-16 17:43:30 +0900

    added import token

--- client/src/views/Import.vue
+++ client/src/views/Import.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="page-content">
-    Importing $OSMO to Metamask 🦊
+    Importing $OSMO
+    <br/>
+    Please check your Metamask 🦊
   </div>
 </template>
 
 <script>
 import { loadingStates } from '../mixins/loading-state'
+import web3 from 'web3'
 // import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 // import { Decimal } from "@cosmjs/math"
 // import { Bech32Address } from "@keplr-wallet/cosmos"
@@ -27,7 +30,7 @@ export default {
     async importToken() {
       try {
         // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-        const wasAdded = await this.$ethereum.request({
+        const wasAdded = await window.ethereum.request({
           method: 'wallet_watchAsset',
           params: {
             type: 'ERC20', // Initially only supports ERC20, but eventually more!
@@ -50,9 +53,65 @@ export default {
       }
 
     },
+    async changeNetwork() {
+      const chainId = 9001 // EVMOS Mainnet
+
+      if (window.ethereum.networkVersion !== chainId) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: web3.utils.toHex(chainId) }],
+          })
+        } catch (err) {
+          // This error code indicates that the chain has not been added to MetaMask
+          if (err.code === 4902) {
+            await window.ethereum.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainName: 'Evmos',
+                  chainId: web3.utils.toHex(chainId),
+                  nativeCurrency: {
+                    name: 'EVMOS',
+                    decimals: 18,
+                    symbol: 'EVMOS',
+                  },
+                  rpcUrls: ['https://eth.bd.evmos.org:8545/'],
+                },
+              ],
+            })
+          }
+        }
+      }
+      this.importToken()
+    },
+    handleAccountsChanged(accounts) {
+      this.metamaskAddress = accounts[0]
+      localStorage.setItem('client', this.client)
+      this.changeNetwork()
+    },
+    async connectWallet() {
+      if (
+        typeof window.ethereum !== 'undefined' &&
+        window.ethereum.isMetaMask
+      ) {
+        await window.ethereum
+          .request({ method: 'eth_requestAccounts' })
+          .then(this.handleAccountsChanged)
+          .catch((err) => {
+            if (err.code === 4001) {
+              console.log(err.message)
+            } else {
+              console.error(err)
+            }
+          })
+      } else {
+        alert('please install MetaMask')
+      }
+    },
   },
-  mounted() {
-    this.importToken()
+  async mounted() {
+    await this.connectWallet()
   },
 }
 </script>
