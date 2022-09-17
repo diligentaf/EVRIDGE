95f8cfa0fce8f464dd53549473599acd3f3c0998
moog.kwon <diligentaf@gmail.com>
2022-09-18 00:09:51 +0900

    ready to submit

--- client/src/views/Explorer.vue
+++ client/src/views/Explorer.vue
@@ -9,12 +9,18 @@
           <v-text-field v-model="search" label="Search by Address, Currency, and TxHash" class="mx-4"></v-text-field>
         </template>
         <template v-slot:item.action="{ item }">
-          <v-btn @click="openEvmosScan(item.metamaskExplorer)">🦊</v-btn>
+          <v-btn @click="openEvmosScan(item.metamaskExplorer)">🦊</v-btn> &ensp;
           <v-btn @click="openMintScan(item.keplrExplorer)">🪐</v-btn>
         </template>
         <template v-slot:[`item.amount`]="{ item }">{{ item.amount }} OSMO</template>
         <template v-slot:[`item.direction`]="{ item }">
             {{ item.direction}}{{item.direction}}{{item.direction}}<br>
+        </template>
+        <template v-slot:[`item.keplrAddress`]="{ item }">
+          <v-btn @click="openCosmosAccount(item.keplrAddress)">{{item.keplrAddress}}</v-btn>
+        </template>
+        <template v-slot:[`item.metamaskAddress`]="{ item }">
+          <v-btn @click="openEvmosAccount(item.metamaskAddress)">{{item.metamaskAddress}}</v-btn>
         </template>
       </v-data-table>
 
@@ -43,16 +49,22 @@ export default {
     openMintScan(item) {
       window.open(`https://www.mintscan.io/osmosis/txs/${item}`, "_blank")
     },
+    openCosmosAccount(item) {
+      window.open(`https://www.mintscan.io/osmosis/account/${item}`, "_blank")
+    },
+    openEvmosAccount(item) {
+      window.open(`https://evm.evmos.org/address/${item}/transactions`, "_blank")
+    },
   },
   computed: {
     headers() {
       return [
         { text: "open explorers", value: "action" },
-        { text: 'amount', value: 'amount' },
+        { text: 'amount', value: 'amount', width: '5%' },
         { text: 'keplrAddress', value: 'keplrAddress' },
-        { text: 'direction', value: 'direction' },
+        { text: 'direction', value: 'direction', width: '5%' },
         { text: 'metamaskAddress', value: 'metamaskAddress' },
-        { text: 'transactions occured', value: 'createdAt' },
+        { text: 'transactions occured', value: 'createdAt', width: '5%'},
       ]
     },
   },
